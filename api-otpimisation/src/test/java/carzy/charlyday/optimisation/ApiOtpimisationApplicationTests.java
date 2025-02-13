4f92f1941ee14d155499cbf4d537f6a7c1431f12
Louka Mellano <72651190+theilook@users.noreply.github.com>
2025-02-13 09:40:11 +0100

    Init

---
+++ api-otpimisation/src/test/java/carzy/charlyday/optimisation/ApiOtpimisationApplicationTests.java
@@ -0,0 +1,13 @@
+package carzy.charlyday.optimisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOtpimisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
