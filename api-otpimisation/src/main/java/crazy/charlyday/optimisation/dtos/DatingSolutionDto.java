ac56750629440312d2a9b87dc3030a2c05caa743
Louka Mellano <72651190+theilook@users.noreply.github.com>
2025-02-13 11:42:07 +0100

    Mappers et dto

---
+++ api-otpimisation/src/main/java/crazy/charlyday/optimisation/dtos/DatingSolutionDto.java
@@ -0,0 +1,17 @@
+package crazy.charlyday.optimisation.dtos;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+public class DatingSolutionDto {
+    @Schema(name = "score", example = "1")
+    private int score;
+
+    @Schema(name = "assignations", example = "{Salarie1: {client: Hugues, skill: JD}}")
+    private Map<SalarieDto, BesoinDto> assignations;
+}
