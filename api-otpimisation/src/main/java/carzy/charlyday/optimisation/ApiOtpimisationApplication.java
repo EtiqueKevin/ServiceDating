0c486922f2a5ac2fea968497eb75aa3eb2a94f54
Louka Mellano <72651190+theilook@users.noreply.github.com>
2025-02-13 09:39:51 +0100

    Init

---
+++ api-otpimisation/src/main/java/carzy/charlyday/optimisation/ApiOtpimisationApplication.java
@@ -0,0 +1,13 @@
+package carzy.charlyday.optimisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOtpimisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOtpimisationApplication.class, args);
+	}
+
+}
