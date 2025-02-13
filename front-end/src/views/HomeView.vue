f2b402bdaeac4cd59455c4cb37505069b1e5b385
Z0ROZ <fabioreveillard@gmail.com>
2025-02-13 12:01:27 +0100

    Front - Accueil et Besoin

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -4,9 +4,27 @@
 
 <template>
   <main>
-    <h1>Home</h1>
+    <h1 class="title">Bienvenue sur ServiceDating</h1>
+    <h2 class="second-title">Ce site web a été conçu dans le cadre du Crazy Charly Day</h2>
   </main>
 </template>
 
 <style scoped>
+
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.title {
+  text-align: center;
+}
+
+.second-title {
+  text-align: center;
+}
+
 </style>
