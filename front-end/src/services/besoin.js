cee97eca227e67b2c9c9026c5b57b231a0586ca3
Z0ROZ <fabioreveillard@gmail.com>
2025-02-13 12:26:58 +0100

    Front - Service Besoin

--- front-end/src/services/besoin.js
+++ front-end/src/services/besoin.js
@@ -1,4 +1,28 @@
+import {inject} from "vue";
+import {useToast} from 'vue-toastification';
 
 
 export const useBesoin = () => {
+
+    const toast = useToast();
+    const api = inject("api");
+
+    const loadCompetence = async () => {
+        try {
+            const res = await this.api.get('competences');
+            return res.data;
+        } catch (err) {
+            toast.error('Erreur lors de la récupération des compétences !')
+        }
+    }
+
+    const postBesoin = async (data) => {
+        try {
+            const res = await this.api.post('besoin', data);
+            return true;
+        } catch (err) {
+            toast.error('Erreur lors de la mise en ligne de votre besoin');
+            return false;
+        }
+    }
 }
