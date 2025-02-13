18b98b96d7ec378eaee4075931cd1ccb515ce773
Aliec-AQ <amaury.quilliec@icloud.com>
2025-02-13 12:33:51 +0100

    ajout admin

---
+++ front-end/src/services/admin.js
@@ -0,0 +1,70 @@
+import { inject } from 'vue'
+
+export const useAdmin = () => {
+    const api = inject('api');
+
+    const getAllBesoins = async () => {
+        try {
+            const res = await api.get('admin/besoins');
+            return res.data;
+        } catch (e) {
+            return [];
+        }
+    }
+
+    const createSalarie = async (data) => {
+        try {
+            await api.post('admin/salaries', data);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    const getAllSalaries = async () => {
+        try {
+            const res = await api.get('admin/salaries');
+            return res.data;
+        } catch (e) {
+            return [];
+        }
+    }
+
+    const getAllCompetences = async () => {
+        try {
+            const res = await api.get('admin/competences');
+            return res.data;
+        } catch (e) {
+            return [];
+        }
+    }
+
+    const createCompetence = async (data) => {
+        try {
+            await api.post('admin/competences', data);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    const updateCompetence = async (id, data) => {
+        try {
+            await api.put(`admin/competences/${id}`, data);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    const deleteCompetence = async (id) => {
+        try {
+            await api.delete(`admin/competences/${id}`);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    
+}
---
+++ front-end/src/views/admin/AdminBesoinsView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="besoins-list">
+        <h1>Liste des Besoins</h1>
+    </div>
+</template>
+
+<style scoped>
+.besoins-list {
+    padding: 20px;
+}
+
+.badge {
+    padding: 8px;
+}
+</style>
