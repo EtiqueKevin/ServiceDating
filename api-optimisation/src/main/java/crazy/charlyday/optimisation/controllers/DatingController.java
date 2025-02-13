8937171020679575b424be92a0bd8b0d9edb6599
Louka Mellano <72651190+theilook@users.noreply.github.com>
2025-02-13 22:33:48 +0100

    Fix CORS

--- api-optimisation/src/main/java/crazy/charlyday/optimisation/controllers/DatingController.java
+++ api-optimisation/src/main/java/crazy/charlyday/optimisation/controllers/DatingController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static crazy.charlyday.optimisation.interfaces.Solvers.*;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/affectations")
 public class DatingController {
